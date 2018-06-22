e0ab8b1c4000d282e927a2c3b155115a0f29c2ad
William Reed <wreed58@gmail.com>
2018-06-22 13:19:09 -0400

    preview and subscribe via search

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -3,6 +3,11 @@ import {Reminder} from '../models/reminder';
 import {Recurring} from '../models/recurring';
 import {ActivatedRoute} from '@angular/router';
 import {SearchService} from '../services/search.service';
+import {UserService} from '../services/user.service';
+import {SubscriptionService} from '../services/subscription.service';
+import {Subscription} from '../models/subscription';
+import {AlertManager} from '../alert/alert.manager';
+import {RecurringService} from '../services/recurring.service';
 
 @Component({
   selector: 'app-search-page',
@@ -13,9 +18,19 @@ export class SearchPageComponent implements OnInit {
 
   reminders: Reminder[] = [];
   recurring: Recurring[] = [];
+  subscriptions: Subscription[];
+  selectedRecurring: Recurring = new Recurring();
   query = '';
+  alertManager = new AlertManager();
 
-  constructor(private route: ActivatedRoute, private searchService: SearchService) {
+  previewTitle = '';
+  previewText = '';
+
+  constructor(private route: ActivatedRoute,
+              private searchService: SearchService,
+              private userService: UserService,
+              private subscriptionService: SubscriptionService,
+              private recurringService: RecurringService) {
   }
 
   ngOnInit() {
@@ -25,8 +40,52 @@ export class SearchPageComponent implements OnInit {
         .then((res) => {
           this.reminders = res.reminders;
           this.recurring = res.recurring;
+
+          this.getSubscriptions();
         });
     });
+  }
+
+  getSubscriptions() {
+    this.userService.loggedIn()
+      .then(loggedIn => {
+        if (loggedIn) {
+          this.subscriptionService.getSubscriptionsForUser()
+            .then((res) => this.subscriptions = res);
+        }
+      });
+  }
+
+  isUserSubscribed(recurring: Recurring) {
+    if (this.subscriptions === undefined) {
+      return;
+    }
+    return this.subscriptions.filter(sub => sub.recurringReminder === recurring.title)[0];
+  }
+
+  handleRecurringClicked(recurring) {
+    this.selectedRecurring = recurring;
+  }
+
+  handleUnsubscribe(event) {
+    this.ngOnInit();
+    this.alertManager.addSuccessAlert('Unsubscribed from ' + event);
+  }
+
+  handlePreviewClicked(recurring) {
+    this.recurringService.getExample(recurring.title).then((res) => {
+      this.previewText = res;
+      this.previewTitle = recurring.title;
+    });
+  }
+
+  handleModalClose(event) {
+    if (event) {
+      this.alertManager.addSuccessAlert(event);
+    }
+    this.selectedRecurring = new Recurring();
+
+    this.ngOnInit();
   }
 
 }
